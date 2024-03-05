0e495fbcaca6a112f5b679dd7ed8277720a657ab
Abdessamad Bouih <151373460+abdessamad10bouih@users.noreply.github.com>
2024-03-05 13:07:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let gateRight = document.getElementById('right-gate');
 
 window.addEventListener('scroll', () =>{
     let value = window.scrollY;
-    text.style.marginTop = value * 2.5 + "px";
+    text.style.marginTop = value * 1.5 + "px";
     treeLeft.style.left = value * -1.5 + "px";
     treeRight.style.left = value * 1.5 + "px";
     gateLeft.style.left = value * -1.5 + "px";
